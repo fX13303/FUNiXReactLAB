b039b76dd7095547932909f36895ede320039d32
Do Duc Hong Phuc <phucddhfx13303@funix.edu.vn>
2022-01-02 16:29:24 +0700

    first commit

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
+
+const Menu = ({ dishes, onClick }) => {
+  const menu = dishes.map(dish => {
+    return (
+      <div className="col-12 col-md-5 m-1">
+        <Card key={dish.id} onClick={() => onClick(dish.id)}>
+          <CardImg width="100%" src={dish.image} alt={dish.name} />
+          <CardImgOverlay>
+            <CardTitle>{dish.name}</CardTitle>
+          </CardImgOverlay>
+        </Card>
+      </div>
+    );
+  });
+  return (
+    <div className="container">
+      <div className="row">{menu}</div>
+    </div>
+  );
+};
+
+export default Menu;
