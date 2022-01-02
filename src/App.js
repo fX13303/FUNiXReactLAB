8426bfebbe2b1ff6308bb8831d8ea599396ed235
Do Duc Hong Phuc <phucddhfx13303@funix.edu.vn>
2022-01-02 16:58:15 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Main from './components/MainComponent';
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Main />
+      </div>
+    );
+  }
+}
+export default App;
