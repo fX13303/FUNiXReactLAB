84e5777bf960f27992dd6aa13b1312a14e13a993
Do Duc Hong Phuc <phucddhfx13303@funix.edu.vn>
2022-01-05 10:05:38 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Main from './components/MainComponent';
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+import {BrowserRouter} from 'react-router-dom';
+
+class App extends Component {
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
+      
+    );
+  }
+}
+export default App;
